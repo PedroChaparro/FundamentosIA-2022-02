c494f20612dd63b376f364aa5c2dd0bfcb431a27
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2022-08-09 13:41:09 -0500

    Add scatter plot
    Also use black formatter

--- 1_abalone/back-end/abalone_controllers.py
+++ 1_abalone/back-end/abalone_controllers.py
@@ -1,86 +1,103 @@
 import pandas as pd
-import matplotlib.pyplot as plot
+import numpy as np
 import base64
 import os
 from scipy import stats
 
-#import numpy as np
-#from scipy import stats
+import matplotlib
+import matplotlib.pyplot as plot
+
+matplotlib.use("Agg")  # Fix main thread error
+
+# import numpy as np
+# from scipy import stats
 
 # Read csv file
-columns=[
-    "SEX", "LENGTH", "DIAMETER", 
-    "HEIGHT", "WHOLE_WEIGHT", "SHUCKED_WEIGHT", 
-    "VISCERA_WEIGHT", "SHELL_WEIGHT", "RINGS"
-    ] 
+columns = [
+    "SEX",
+    "LENGTH",
+    "DIAMETER",
+    "HEIGHT",
+    "WHOLE_WEIGHT",
+    "SHUCKED_WEIGHT",
+    "VISCERA_WEIGHT",
+    "SHELL_WEIGHT",
+    "RINGS",
+]
 
 data_frame = pd.read_csv("abalone.csv", names=columns)
+
 
 def encode_last_generated_to_base64():
     encoded_string = ""
 
-    # Parse to base64 and return the string        
+    # Parse to base64 and return the string
     with open("./cache/last_generated.jpg", "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
-        
+
     # Remove the tempfile
     os.remove("./cache/last_generated.jpg")
 
     return encoded_string
 
+
 def generateHistogramFromColum(column):
-    if(column.upper() in columns):
+    if column.upper() in columns:
         # Create the histogram
         plot.hist(
-            x = data_frame[column.upper()], 
-            density=True, 
-            color="#C1E7FC", 
-            edgecolor="black"
+            x=data_frame[column.upper()],
+            density=True,
+            color="#C1E7FC",
+            edgecolor="black",
         )
 
         # Save in the new folder
         plot.savefig("./cache/last_generated.jpg")
 
-        #Encode
+        # Encode
         encoded_string = encode_last_generated_to_base64()
         return encoded_string
-    else: 
+    else:
         return "Column does not exists"
+
 
 def generateBoxPlotFromColumn(column):
-    if(column.upper() in columns):
+    if column.upper() in columns:
         # Create the boxplot
-        plot.boxplot(x = data_frame[column.upper()])
+        plot.boxplot(x=data_frame[column.upper()])
 
         # Save in the new folder
         plot.savefig("./cache/last_generated.jpg")
 
-        #Encode
+        # Encode
         encoded_string = encode_last_generated_to_base64()
         return encoded_string
-    else: 
+    else:
         return "Column does not exists"
+
 
 def generateNormPlotFromColumn(column):
-    if(column.upper() in columns):
-        # Create the normplot
-        normal_prob_figure = plot.figure()
+    if column.upper() in column:
 
-        ax = normal_prob_figure.add_subplot(111) # One data type
-        
-        res = stats.probplot(
-            data_frame[column.upper()], 
-            dist=stats.norm, 
-            sparams=(6, ), # Media de 6, desviación estándar de 1
-            plot=ax
+        print("Hi")
+    else:
+        print("Bye")
+
+
+def generateScatterPlotFromColumns(column1, column2):
+    if column1.upper() in columns and column2.upper() in columns:
+        # Create the boxplot
+        plot.scatter(
+            np.array(data_frame[column1.upper()]),
+            np.array(data_frame[column2.upper()]),
+            edgecolor="black",
         )
-        plot.subplots()
 
         # Save in the new folder
         plot.savefig("./cache/last_generated.jpg")
 
-        #Encode
+        # Encode
         encoded_string = encode_last_generated_to_base64()
         return encoded_string
-    else: 
-        return "Column does not exists"
+    else:
+        return "One (or both) of the columns doest not exits"
--- 1_abalone/back-end/app.py
+++ 1_abalone/back-end/app.py
@@ -1,45 +1,62 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 import abalone_controllers
 
 app = Flask(__name__)
 
 # Routes
+@app.route("/histogram", methods=["POST"])
+def histograms():
+    # Get json data
+    request_data = request.json
 
-@app.route("/histogram/<string:column>")
-def histograms(column):
+    base64_plot = abalone_controllers.generateHistogramFromColum(request_data["column"])
+
+    return_dict = {"image": "{}".format(base64_plot)}
+
     # Parse to json format
-    base64_plot = abalone_controllers.generateHistogramFromColum(column)
-    return_dict = {
-        "image": "{}".format(base64_plot)
-    }
+    return jsonify(return_dict)
+
+
+@app.route("/boxplot", methods=["POST"])
+def box_plots():
+    # Get json data
+    request_data = request.json
+
+    # Parse to json format
+    base64_plot = abalone_controllers.generateBoxPlotFromColumn(request_data["column"])
+
+    return_dict = {"image": "{}".format(base64_plot)}
 
     return jsonify(return_dict)
 
-@app.route("/boxplot/<string:column>")
-def box_plots(column):
-    # Parse to json format
-    base64_plot = abalone_controllers.generateBoxPlotFromColumn(column)
 
-    return_dict = {
-        "image": "{}".format(base64_plot)
-    }
+@app.route("/normplot", methods=["POST"])
+def norm_plots():
+    # Get json data
+    request_data = request.json
+
+    # Parse to json format
+    base64_plot = abalone_controllers.generateNormPlotFromColumn(request_data["column"])
+
+    return_dict = {"image": "{}".format(base64_plot)}
 
     return jsonify(return_dict)
 
-@app.route("/normplot/<string:column>")
-def norm_plots(column):
-    # Parse to json format
-    base64_plot = abalone_controllers.generateNormPlotFromColumn(column)
-    
-    return_dict = {
-        "image": "{}".format(base64_plot)
-    }
 
-    return jsonify(return_dict)
-
-@app.route("/scatter")
+@app.route("/scatter", methods=["POST"])
 def scatter_plots():
-    return abalone_controllers.generateHistogramFromColum()
+    # Get json data
+    request_data = request.json
+
+    # Parse to json format
+    base64_plot = abalone_controllers.generateScatterPlotFromColumns(
+        request_data["column1"], request_data["column2"]
+    )
+
+    return_dict = {"image": "{}".format(base64_plot)}
+
+    return jsonify(return_dict)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
